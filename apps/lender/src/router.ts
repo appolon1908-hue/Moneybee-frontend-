692b14db9bff2b34778d28f78107646891ef19a9
Ralph Appolon <appolon1908@gmail.com>
2026-08-23 11:29:45 -0400

    route lender submissions

--- apps/lender/src/router.ts
+++ apps/lender/src/router.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router"
 import DashboardView from "./views/DashboardView.vue"
+import SubmissionsView from "./views/SubmissionsView.vue"
 export default createRouter({history: createWebHistory(), routes: [
   {path: "/", redirect: "/dashboard"}, {path: "/dashboard", component: DashboardView},
-  {path: "/applications", component: DashboardView}, {path: "/programs", component: DashboardView},
-  {path: "/offers", component: DashboardView}, {path: "/funded-deals", component: DashboardView},
+  {path: "/applications", component: SubmissionsView}, {path: "/programs", component: DashboardView},
+  {path: "/offers", component: SubmissionsView}, {path: "/funded-deals", component: DashboardView},
 ]})
