dffee863eff97c0140c7f80c93c71665a17c6459
Ralph Appolon <appolon1908@gmail.com>
2026-08-23 11:29:42 -0400

    add borrower lifecycle navigation

--- apps/borrower/src/App.vue
+++ apps/borrower/src/App.vue
@@ -2,8 +2,8 @@
   <div class="portal">
     <aside class="sidebar"><div class="brand"><span class="mark">MB</span> MoneyBee</div>
       <nav><RouterLink to="/dashboard">Dashboard</RouterLink><RouterLink to="/application">Application</RouterLink>
-        <RouterLink to="/documents">Documents</RouterLink><RouterLink to="/banking">Bank connection</RouterLink>
-        <RouterLink to="/offers">Funding offers</RouterLink></nav>
+        <RouterLink to="/conditions">Conditions</RouterLink><RouterLink to="/offers">Funding offers</RouterLink>
+        <RouterLink to="/support">Support</RouterLink></nav>
     </aside>
     <main class="content"><RouterView /></main>
   </div>
