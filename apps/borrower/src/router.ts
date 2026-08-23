51bb5745dc2326d618d2cb322b54e464a5e5183a
Ralph Appolon <appolon1908@gmail.com>
2026-08-23 11:29:41 -0400

    route borrower conditions and support

--- apps/borrower/src/router.ts
+++ apps/borrower/src/router.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router"
 import ApplicationView from "./views/ApplicationView.vue"
+import ConditionsView from "./views/ConditionsView.vue"
 import DashboardView from "./views/DashboardView.vue"
 import OffersView from "./views/OffersView.vue"
+import SupportView from "./views/SupportView.vue"
 
 export default createRouter({
   history: createWebHistory(),
@@ -10,6 +12,8 @@ export default createRouter({
     {path: "/dashboard", component: DashboardView},
     {path: "/offers", component: OffersView},
     {path: "/application", component: ApplicationView},
+    {path: "/conditions", component: ConditionsView},
+    {path: "/support", component: SupportView},
     {path: "/documents", component: DashboardView},
     {path: "/banking", component: DashboardView},
   ],
